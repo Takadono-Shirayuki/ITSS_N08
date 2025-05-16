896e743bcc895a89fb6ac018a8175d999bb8d654
Takadono-Shirayuki <Thanh.BT225669@sis.hust.edu.vn>
2025-05-16 21:49:30 +0700

    Khởi tạo dự án

---
+++ ITSS_08_Pet care center management system/View Controller/Form1.cs
@@ -0,0 +1,15 @@
+namespace ITSS_08_Pet_care_center_management_system
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
