246b1d4cd12dc84b9d25433b01d69e0f1365f56f
Takadono-Shirayuki <Thanh.BT225669@sis.hust.edu.vn>
2025-05-20 02:36:15 +0700

    khởi tạo dự án

---
+++ Server/Program.cs
@@ -0,0 +1,23 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Thêm cách dịch vụ vào container.
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection(); // Chuyển hướng HTTP sang HTTPS
+app.UseRouting(); // Định tuyến yêu cầu đến các endpoint
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers(); // Định tuyến các controller 
+});
+
+app.Run();
